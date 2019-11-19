93a91156b9ddc6785164851154c4641af64e9809
lumenbarron <lucemendez@gmail.com>
2019-11-18 21:45:49 -0600

    Updates

---
+++ precache-manifest.c971bd489fe439ce2b5ab7a737946e32.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe72e27a309b17ab5b153e1006bf2163",
+    "url": "/pixar/index.html"
+  },
+  {
+    "revision": "c010c81a2ba3f857c54f",
+    "url": "/pixar/static/css/2.84e3cfba.chunk.css"
+  },
+  {
+    "revision": "e8883221d05bd4283eb1",
+    "url": "/pixar/static/css/main.bb72f829.chunk.css"
+  },
+  {
+    "revision": "c010c81a2ba3f857c54f",
+    "url": "/pixar/static/js/2.73c96d0a.chunk.js"
+  },
+  {
+    "revision": "e8883221d05bd4283eb1",
+    "url": "/pixar/static/js/main.682e66cc.chunk.js"
+  },
+  {
+    "revision": "fd442037562f95cd353c",
+    "url": "/pixar/static/js/runtime-main.7f2f5ab2.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/pixar/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/pixar/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/pixar/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/pixar/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/pixar/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
